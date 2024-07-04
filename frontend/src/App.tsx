fdf3cfd9ae806bbeb8f25aaa2a91f90e939dab95
locodafux <locodafux@gmail.com>
2024-07-04 19:38:53 +0800

    changed title and removed some unnecessary things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { DarkThemeToggle } from "flowbite-react";
 import { MyNav } from "./components/NavBar.tsx";
 
 function App() {
   return (
     <main className="flex w-screen h-screen dark:bg-gray-800">
         <MyNav />
-        <DarkThemeToggle className="h-fit"/>
     </main>
   );
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,25 +1,13 @@
 
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { Navbar } from "flowbite-react";  
+import { DarkThemeToggle } from "flowbite-react";
 
 export function MyNav() {
   return (
-    <Navbar fluid rounded>
-      <NavbarBrand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </NavbarBrand>
-      <NavbarToggle />
-      <NavbarCollapse>
-        <NavbarLink href="#" active>
-          Home
-        </NavbarLink>
-        <NavbarLink  href="#">
-          About
-        </NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
-      </NavbarCollapse>
+    <Navbar className="w-screen h-16 flex ">
+   
+     <DarkThemeToggle className="h-fit ml-auto"/>
+
     </Navbar>
   );
 }
