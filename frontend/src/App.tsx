55561186b0314f55a23829a20e92cd304911f30a
locodafux <locodafux@gmail.com>
2024-07-04 19:16:59 +0800

    added navbar but not finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { DarkThemeToggle } from "flowbite-react";
+import { MyNav } from "./components/NavBar.tsx";
 
 function App() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
-      <DarkThemeToggle />
+    <main className="flex w-screen h-screen dark:bg-gray-800">
+        <MyNav />
+        <DarkThemeToggle className="h-fit"/>
     </main>
   );
 }
